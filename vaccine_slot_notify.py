1b8024382996d2b21b931f4e9e8a366dc3173a8b
VikasDamodar <vikas9142@gmail.com>
2021-07-13 11:11:07 +0530

    Update vaccine_slot_notify.py

--- vaccine_slot_notify.py
+++ vaccine_slot_notify.py
@@ -3,43 +3,47 @@ import datetime
 from twilio.rest import Client
 
 
-district_kasaragod_id = 295
-date = str(datetime.datetime.today().date().strftime("%d-%m-%Y"))
+def vaccine_notifier():
+    district_kasaragod_id = 295
+    date = str(datetime.datetime.today().date().strftime("%d-%m-%Y"))
 
-params = {
-    "district_id": district_kasaragod_id,
-    "date": date
-}
-vaccine_centres = requests.get(
-    "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict",
-    params=params)
+    params = {
+        "district_id": district_kasaragod_id,
+        "date": date
+    }
+    vaccine_centres = requests.get(
+        "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict",
+        params=params)
 
-message_body = None
-for each in vaccine_centres.json()['centers']:
-    for each_centre in each["sessions"]:
-        centre_name = each['name']
-        availability = each_centre['available_capacity_dose1']
-        vaccine_name = each_centre['vaccine']
-        age_group = each_centre['min_age_limit']
-        date_available = each_centre['date']
-        if age_group == 18 and availability > 0:
-            message_body += "Vaccine available at " + centre_name + " " +\
-                            vaccine_name + "Available "\
-                            + str(availability) + " On " + date_available
-            print(message_body)
+    message_body = None
+    for each in vaccine_centres.json()['centers']:
+        for each_centre in each["sessions"]:
+            centre_name = each['name']
+            availability = each_centre['available_capacity_dose1']
+            vaccine_name = each_centre['vaccine']
+            age_group = each_centre['min_age_limit']
+            date_available = each_centre['date']
+            if age_group == 18 and availability > 0:
+                message_body += "Vaccine available at " + centre_name + " " +\
+                                vaccine_name + "Available "\
+                                + str(availability) + " On " + date_available
+                print(message_body)
+
+    account_sid = "ACe125b3fc8e803f2960274396759a230f"
+    auth_token = "33013a46c13cd0a5748acb1e4e655665"
+    client = Client(account_sid, auth_token)
+    print(message_body)
+    if message_body:
+        message = client.messages.create(
+                             body="TEst",
+                             from_='+18186166179',
+                             to='+918086637696'
+                         )
+
+        print(message.sid)
 
 
-account_sid = "ACe125b3fc8e803f2960274396759a230f"
-auth_token = "33013a46c13cd0a5748acb1e4e655665"
-client = Client(account_sid, auth_token)
-print(message_body)
-if message_body:
-    message = client.messages \
-                    .create(
-                         body="TEst",
-                         from_='+18186166179',
-                         to='+918086637696'
-                     )
+if __name__ == '__main__':
+    vaccine_notifier()
 
-    print(message.sid)
 
